660f64e9aa74af335882c6166232d979d98d81c9
Lawrence <lawrence.ley@contextsolutions.ca>
2025-04-14 10:37:58 -0400

    feat: moved seed phrase to use env variables

--- test/client.ts
+++ test/client.ts
@@ -37,11 +37,10 @@ if (emulator) {
   wallet = (client as NetworkEmulator).createWallet(10_000_000n);
   (client as NetworkEmulator).tick(10n);
 } else {
-  const entropy =
-    "ridge cereal poet happy borrow melody fashion donor amateur calm erupt horror traffic onion crack brick cycle dawn wall thing census parent bachelor next";
+  const entropy = process.env.WALLET_ENTROPY || "";
   const rootKey = RootPrivateKey.fromPhrase(entropy.split(" "));
   const privateKey = rootKey.deriveSpendingKey();
   wallet = new SimpleWallet(client, privateKey);
   console.log("Wallet created");
-  console.log(wallet.address.toBech32());
+  console.log("Wallet address: ", wallet.address.toBech32());
 }
